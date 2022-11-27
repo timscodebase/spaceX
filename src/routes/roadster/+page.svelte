59f2059154ce3fb131f334e045ff3d2dc3b22972
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-11-26 16:13:52 -0800

    css

--- src/routes/roadster/+page.svelte
+++ src/routes/roadster/+page.svelte
@@ -97,7 +97,7 @@
 
 	@media (max-width: 600px) {
 		.grid {
-			grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+			grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 		}
 	}
 
